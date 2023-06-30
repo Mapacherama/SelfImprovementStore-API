456a42f2d6497ea2a98a2f8d53a69943e48f57b1
Mapacherama <48964493+Mapacherama@users.noreply.github.com>
2023-06-30 13:17:01 +0200

    Feature: Changed the launchsettings to graphql and started configurating GraphQL

---
+++ Self-help-Webstore-GraphQL-API/Self-help-Webstore-GraphQL-API.Web/SelhelpWebstoreDBContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Self_help_Webstore_GraphQL_API.src.Self_help_Webstore_GraphQL_API.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace Self_help_Webstore_GraphQL_API.Self_help_Webstore_GraphQL_API.Web
+{
+    public class SelhelpWebstoreDBContext : DbContext
+    {
+        public SelhelpWebstoreDBContext(DbContextOptions<SelhelpWebstoreDBContext> options)
+    : base(options)
+        { }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}
--- Self-help-Webstore-GraphQL-API/Self-help-Webstore-GraphQL-API.Web/Startup.cs
+++ Self-help-Webstore-GraphQL-API/Self-help-Webstore-GraphQL-API.Web/Startup.cs
@@ -1,12 +1,23 @@
-﻿using Self_help_Webstore_GraphQL_API.src.Self_help_Webstore_GraphQL_API;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Self_help_Webstore_GraphQL_API.src.Self_help_Webstore_GraphQL_API;
 
 namespace Self_help_Webstore_GraphQL_API.Self_help_Webstore_GraphQL_API.Web
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
             services
+        .AddDbContext<SelhelpWebstoreDBContext>(options =>
+            options.UseNpgsql(Configuration.GetConnectionString("DefaultConnection")))
             .AddGraphQLServer()
                 .AddQueryType<Query>()
                 .AddMutationType<Mutation>();
